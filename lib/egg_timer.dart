fa092b127d6de723e3ff9cd904f3387ff0ab35e7
José Beltrán <jobel.code@gmail.com>
2018-06-09 16:03:07 +0200

    Added Pause button

--- lib/egg_timer.dart
+++ lib/egg_timer.dart
@@ -1,9 +1,11 @@
+// Inspired by https://www.youtube.com/watch?v=svxUUz5mi9s&t=2147s
+
 import 'package:flutter/material.dart';
 import 'package:fluttery/framing.dart';
 import 'egg_timer_time_display.dart';
 
-final Color GRADIENT_TOP = const Color(0xFFF5F5F5);
-final Color GRADIENT_BOTTOM = const Color(0xFFE8E8E8);
+//final Color GRADIENT_TOP = const Color(0xFFF5F5F5);
+//final Color GRADIENT_BOTTOM = const Color(0xFFE8E8E8);
 
 class MyApp extends StatelessWidget {
   @override
@@ -29,16 +31,34 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               ),
-              Expanded(child: Container(),
+              Expanded(
+                child: Container(),
               ),
 
               RandomColorBlock(
                 width: double.infinity,
                 height: 50.0,
               ),
-              RandomColorBlock(
-                width: double.infinity,
-                height: 50.0,
+              FlatButton(
+                splashColor: Color(0x22000000),
+                onPressed: () {},
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.pause,
+                      color: Colors.black,
+                    ),
+                    Text(
+                      'Pause',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 3.0,
+                      ),
+                    )
+                  ],
+                ),
               ), // From Fluttery/framing
             ],
           ),
