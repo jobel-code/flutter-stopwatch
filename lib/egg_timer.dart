98701a00553e804199384c6298efbdaa156a0f9f
José Beltrán <jobel.code@gmail.com>
2018-06-11 09:07:01 +0200

    BoxShape.circle with shadow!

--- lib/egg_timer.dart
+++ lib/egg_timer.dart
@@ -6,8 +6,8 @@ import 'package:stopwatch/egg_timer_controls.dart';
 import 'package:stopwatch/egg_timer_time_display.dart';
 import 'package:stopwatch/egg_timer_button.dart';
 
-//final Color GRADIENT_TOP = const Color(0xFFF5F5F5);
-//final Color GRADIENT_BOTTOM = const Color(0xFFE8E8E8);
+final Color GRADIENT_TOP = const Color(0xFFF5F5F5);
+final Color GRADIENT_BOTTOM = const Color(0xFFE8E8E8);
 
 class MyApp extends StatelessWidget {
   @override
@@ -17,30 +17,52 @@ class MyApp extends StatelessWidget {
         fontFamily: 'BebasNeue',
       ),
       home: Scaffold(
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              EggTimerTimeDisplay(),
-              RandomColorBlock(
-                width: double.infinity,
-                child: Padding(
-                  padding: EdgeInsets.only(left: 45.0, right: 45.0),
-                  child: AspectRatio(
-                    aspectRatio: 1.0,
-                    child: RandomColorBlock(
-                      width: double.infinity,
-                    ),
+        // Container used to keep the color gradient of the background
+        body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [GRADIENT_TOP, GRADIENT_BOTTOM])),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                EggTimerTimeDisplay(),
+                Container(
+                  width: double.infinity,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 45.0, right: 45.0),
+                    child: AspectRatio(
+                        aspectRatio: 1.0,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            gradient: LinearGradient(
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                                colors: [GRADIENT_TOP, GRADIENT_BOTTOM]),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color(0x44000000),
+                                blurRadius: 2.0,
+                                spreadRadius: 1.0,
+                                offset: Offset(0.0, 1.0),
+                              )
+                            ]
+                          ),
+                          child: Container(),
+                        )),
                   ),
                 ),
-              ),
-              Expanded(
-                child: Container(),
-              ),
+                Expanded(
+                  child: Container(),
+                ),
 
-              EggTimerControls(),
+                EggTimerControls(),
 
-               // From Fluttery/framing
-            ],
+                // From Fluttery/framing
+              ],
+            ),
           ),
         ),
       ),
