beb896f320bf564c17616162118ba5337d9688a1
José Beltrán <jobel.code@gmail.com>
2018-06-11 09:12:46 +0200

    inner circle

--- lib/egg_timer.dart
+++ lib/egg_timer.dart
@@ -33,25 +33,46 @@ class MyApp extends StatelessWidget {
                   child: Padding(
                     padding: EdgeInsets.only(left: 45.0, right: 45.0),
                     child: AspectRatio(
-                        aspectRatio: 1.0,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            gradient: LinearGradient(
-                                begin: Alignment.topCenter,
-                                end: Alignment.bottomCenter,
-                                colors: [GRADIENT_TOP, GRADIENT_BOTTOM]),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Color(0x44000000),
-                                blurRadius: 2.0,
-                                spreadRadius: 1.0,
-                                offset: Offset(0.0, 1.0),
-                              )
-                            ]
+                      aspectRatio: 1.0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          gradient: LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [GRADIENT_TOP, GRADIENT_BOTTOM]),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color(0x44000000),
+                              blurRadius: 2.0,
+                              spreadRadius: 1.0,
+                              offset: Offset(0.0, 1.0),
+                            ),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(65.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              gradient: LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: [GRADIENT_TOP, GRADIENT_BOTTOM]),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color(0x44000000),
+                                  blurRadius: 2.0,
+                                  spreadRadius: 1.0,
+                                  offset: Offset(0.0, 1.0),
+                                ),
+                              ],
+                            ),
+                            child: Container(),
                           ),
-                          child: Container(),
-                        )),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
                 Expanded(
