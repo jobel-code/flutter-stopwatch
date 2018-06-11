6f3fa73f76257f422bcebc58ce32e034e35a970a
José Beltrán <jobel.code@gmail.com>
2018-06-11 12:32:31 +0200

    Added rotated text (textPainter)

--- lib/egg_timer_dial.dart
+++ lib/egg_timer_dial.dart
@@ -3,8 +3,6 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:stopwatch/egg_timer_knob.dart';
 
-
-
 class EggTimerDial extends StatefulWidget {
   @override
   _EggTimerDialState createState() => _EggTimerDialState();
@@ -36,36 +34,26 @@ class _EggTimerDialState extends State<EggTimerDial> {
               ],
             ),
             // This padding pushes in from the outer circle
-            child: Stack(
-              children: <Widget>[
-                Container(
-                  // this padding controls where the ticks will be pushed from the outer circle
-                  padding: EdgeInsets.all(55.0),
+            child: Stack(children: <Widget>[
+              Container(
+                // this padding controls where the ticks will be pushed from the outer circle
+                padding: EdgeInsets.all(55.0),
 
-                  width: double.infinity,
-                  height: double.infinity,
-                  child: CustomPaint(
-                    painter: TickPainter(
-
-                    )
-                  ),
-                ),
-
-                Padding(
-                  padding: const EdgeInsets.all(65.0),
-                  child: EggTimerDialKnob(),
-
-                ),
-              ]
-            ),
+                width: double.infinity,
+                height: double.infinity,
+                child: CustomPaint(painter: TickPainter()),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(65.0),
+                child: EggTimerDialKnob(),
+              ),
+            ]),
           ),
         ),
       ),
     );
   }
 }
-
-
 
 class TickPainter extends CustomPainter {
   final LONG_TICK = 14.0;
@@ -76,19 +64,28 @@ class TickPainter extends CustomPainter {
   final ticksInset;
   final tickPaint;
 
+  final textPainter;
+  final textStyle;
+
   TickPainter({
     this.tickCount = 35,
     this.ticksPerSection = 5,
     this.ticksInset = 0.0,
-}) : tickPaint = Paint() {
+  })  : tickPaint = Paint(),
+        textPainter = TextPainter(
+          textAlign: TextAlign.center,
+          textDirection:
+              TextDirection.ltr, // do not assume that this is set by default.
+          // It is not, and will raise an error if not set.
+        ),
+        textStyle = TextStyle(
+          color: Colors.black,
+          fontFamily: 'BebasNeue',
+          fontSize: 20.0,
+        ) {
     tickPaint.color = Colors.black;
     tickPaint.strokeWidth = 1.5;
   }
-
-
-
-
-
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -103,33 +100,56 @@ class TickPainter extends CustomPainter {
 
     // Painting short ticks.
     for (var i = 0; i < tickCount; ++i) {
-
       final tickLength = i % ticksPerSection == 0 ? LONG_TICK : SHORT_TICK;
 
       canvas.drawLine(
           Offset(0.0, -radius), // minus radius corrects the orientation
-                                // so the first LONG_TICK goes on top.
+          // so the first LONG_TICK goes on top.
           Offset(0.0, -radius - tickLength), // negative goes up
-                                             // plus keeps it on the exterior
-          tickPaint
-      );
-      
+          // plus keeps it on the exterior
+          tickPaint);
+
+      // Check if LONG_TICK then paint text
+      if (i % ticksPerSection == 0) {
+        // Paint text.
+        canvas.save();
+
+        canvas.translate(0.0, -(size.width /2) -30.0); // painting text up.
+        textPainter.text = TextSpan(
+          text: '$i',
+          style: textStyle,
+        );
+
+        // Layout the text, to figure it out how large the text would be.
+        textPainter.layout();
+
+        textPainter.paint(
+          canvas,
+          // the offset will help to recenter the text, because if it is zero,
+          // then all the text is left-aligned with the tick mark.
+          // we need to offset the text exactly by half the width of the text.
+          Offset(
+              -textPainter.width / 2,
+              // Now to avoid the text touching the tip of the tick, we offset by
+              // half of the text height.
+              -textPainter.height / 2),
+
+        );
+
+
+        canvas.restore();
+
+      }
+
       canvas.rotate(2 * pi / tickCount);
     }
 
     // restore after drawing all ticks
     canvas.restore();
-
-
   }
-  
-  
-  
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
     return true;
   }
-
 }
-
